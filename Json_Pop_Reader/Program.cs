8e6fd4633c63e743a3b3c13a66476a21e63461e2
RapidfireCRH <CRHRapidfire@gmail.com>
2017-11-03 14:59:11 -0600

    Added (f)ind and (c)losest commands

--- Json_Pop_Reader/Program.cs
+++ Json_Pop_Reader/Program.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Diagnostics;
+using System.Threading;
+using System.Collections;
 
 namespace pop_system
 {
     class Program
     {
+        struct distance_template : IComparable<distance_template>
+        {
+            public int place;
+            public float distance;
+            public int CompareTo(distance_template other)
+            {
+                return this.distance.CompareTo(other.distance);
+            }
+        }
         static void Main(string[] args)
         {
             Json_reader j = new Json_reader();
@@ -17,28 +28,67 @@ namespace pop_system
                 for (int scan = 1; scan != systems.Length; scan++)
                     if (systems[ptr].last_scan_date > systems[scan].last_scan_date && systems[scan].done != true)
                         ptr = scan;
-                Console.Clear();
-                Console.WriteLine("Entry" + ptr + ": ID - " + systems[ptr].id + " System Name: " + systems[ptr].name);
-                Console.WriteLine("Last entry: " + epochconvert((int)systems[ptr].last_scan_date));
-                Console.WriteLine("Pop: " + (systems[ptr].population == -1 ? "Unknown" : systems[ptr].population.ToString()) + " System Gov: " + (Json_reader.government_name)systems[ptr].government_type + " Security: " + (Json_reader.security)systems[ptr].security_id);
-                string temp = "";
-                char k = ' ';
-                while (k != 'd' && !systems[ptr].done)
+                string k = "";
+                while (!systems[ptr].done)
                 {
-                    k = (char)Console.Read();
-                    switch (k)
+                    Console.Clear();
+                    Console.WriteLine("Entry" + ptr + ": ID - " + systems[ptr].id + " System Name: " + systems[ptr].name);
+                    Console.WriteLine("Last entry: " + epochconvert((int)systems[ptr].last_scan_date));
+                    Console.WriteLine("Pop: " + (systems[ptr].population == -1 ? "Unknown" : systems[ptr].population.ToString()) + " | System Gov: " + (Json_reader.government_name)systems[ptr].government_type + " | Security: " + (Json_reader.security)systems[ptr].security_id);
+                    k = Console.ReadLine();
+                    if (k.Length == 0)
+                        continue;
+                    switch (k[0])
                     {
                         case 'r':
-                            temp = "https://ross.eddb.io/system/update/" + systems[ptr].id.ToString();
-                            Process.Start(new ProcessStartInfo("cmd", $"/c start {temp}") { CreateNoWindow = true });
+                            System.Diagnostics.Process.Start("https://ross.eddb.io/system/update/" + systems[ptr].id.ToString());
                             break;
                         case 'e':
-                            temp = "https://www.edsm.net/en/system/bodies/id/" + systems[ptr].edsm_id.ToString() + "/name";
-                            Process.Start(new ProcessStartInfo("cmd", $"/c start {temp}") { CreateNoWindow = true });
+                            System.Diagnostics.Process.Start("https://www.edsm.net/en/system/bodies/id/" + systems[ptr].edsm_id.ToString() + "/name");
+                            break;
+                        case 'd':
+                            systems[ptr].done = true;
+                            break;
+                        case 's':
+                            string search = k.Substring(2);
+                            bool found = false;
+                            for(int i = 0; i!=systems.Length;i++)
+                            {
+                                if (systems[i].name.ToLower() == search.ToLower())
+                                {
+                                    ptr = i;
+                                    systems[ptr].done = false;
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            if(!found)
+                            {
+                                Console.WriteLine("");
+                                Console.WriteLine(search + " not found.");
+                                Thread.Sleep(4000);
+                            }
+                            break;
+
+                        case 'c':
+                            List<distance_template> list = new List<distance_template>();
+                            for (int scan = 0; scan != systems.Length; scan++)
+                            {
+                                distance_template next = new distance_template();
+                                next.place = scan;
+                                next.distance = (float)Math.Sqrt(Math.Pow(systems[scan].coordinates[0] - systems[ptr].coordinates[0], 2) + Math.Pow(systems[scan].coordinates[1] - systems[ptr].coordinates[1], 2) + Math.Pow(systems[scan].coordinates[2] - systems[ptr].coordinates[2], 2));
+                                list.Add(next);
+                            }
+                            list.Sort();
+                            Console.WriteLine();
+                            for(int i = 1; i!= 21; i++)
+                                Console.WriteLine(i.ToString() + ". " + Math.Ceiling(list[i].distance) + "ly | " + systems[list[i].place].name + " - Last Updated: " + epochconvert((int)systems[list[i].place].last_scan_date));
+                            Console.WriteLine("press enter to continue");
+                            Console.Read();
                             break;
                     }
                 }
-                systems[ptr].done = true;
+                
             }
         }
         static DateTime epochconvert(int epoch)
